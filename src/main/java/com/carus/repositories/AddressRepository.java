e04c60efb17d3a8817430b5c64d13804d7adca07
Pedro Zwang <pedroh.zwang@gmail.com>
2022-11-06 20:46:56 -0300

    api para buscar os dados do perfil do usuario logado

--- src/main/java/com/carus/repositories/AddressRepository.java
+++ src/main/java/com/carus/repositories/AddressRepository.java
@@ -2,7 +2,12 @@ package com.carus.repositories;
 
 import com.carus.entities.AddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AddressRepository extends JpaRepository<AddressEntity, Long> {}
+public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
+
+    @Query(value = "select * from address where user_id = ?1 limit 1", nativeQuery = true)
+    AddressEntity findAddressByUserId(Long userId);
+}
--- src/main/java/com/carus/repositories/RateUserRepository.java
+++ src/main/java/com/carus/repositories/RateUserRepository.java
@@ -2,8 +2,13 @@ package com.carus.repositories;
 
 import com.carus.entities.RateUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RateUserRepository extends JpaRepository<RateUserEntity, Long> {}
+public interface RateUserRepository extends JpaRepository<RateUserEntity, Long> {
+
+    @Query("select count(r) from RateUserEntity r where r.ratedUser.id = ?1")
+    Long countRatesByUserId(Long userId);
+}
 
