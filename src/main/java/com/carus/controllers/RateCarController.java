331cd205d1961a49faa2f185868e963a5d4f5283
Jefferson <jefmaylon@gmail.com>
2022-11-16 17:39:01 -0300

    Ajustes PR

--- src/main/java/com/carus/controllers/RateCarController.java
+++ src/main/java/com/carus/controllers/RateCarController.java
@@ -26,23 +26,23 @@ public class RateCarController {
     }
 
 
-    @GetMapping(value = "/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<RateCarDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(rateCarService.findById(id));
     }
 
-    @PostMapping(value = "/create")
+    @PostMapping("/create")
     public ResponseEntity<RateCarDTO> save(@RequestBody RateCarDTO dto) {
         return ResponseEntity.ok(rateCarService.save(dto));
     }
 
-    @DeleteMapping(value = "/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         rateCarService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping(value = "/car-by-user/{uuid}")
+    @GetMapping("/user/{uuid}")
     public ResponseEntity<List<RateCarDTO>> findAllByUserUuid(@PathVariable String uuid) {
         return ResponseEntity.ok(rateCarService.findAllByUserUuid(uuid));
     }
