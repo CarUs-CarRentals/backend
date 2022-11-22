92f1582151ab3726111dd1d3ccaa6e4e82396b86
Jefferson <jefmaylon@gmail.com>
2022-11-22 00:21:34 -0300

    Adicionada APIs para buscar as locações por usuário logado e pelo id do carro

--- src/main/java/com/carus/repositories/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.carus.repositories;
-
-import com.carus.entities.RentalEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocationRepository extends JpaRepository<RentalEntity, Long> {}
